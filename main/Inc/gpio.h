96d70413d7fc2647f7757048bb873fd68e69bcf8
The_Platypus <zarazach@protonmail.com>
2022-07-16 01:12:11 +0200

    more stuff

--- main/Inc/gpio.h
+++ main/Inc/gpio.h
@@ -1,6 +1,7 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+//#include <stdio.h>
 #include <stdbool.h>
 
 typedef enum {
@@ -8,6 +9,7 @@ typedef enum {
 	gpio_output
 } gpio_mode_t;
 
+void test();
 
 
 #endif /* GPIO_H_ */
--- main/Src/main.c
+++ main/Src/main.c
@@ -1,6 +1,6 @@
-//#include <stdio.h>
+#include <stdio.h>
 
 void app_main(void)
 {
-
+    
 }
